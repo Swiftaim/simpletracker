da9b638b15133cd96296d613b044c6a7922ac1a0
Swiftaim <david.dalstrom@gmail.com>
2015-05-23 15:34:58 +0200

    Initial app

---
+++ app.py
@@ -0,0 +1,8 @@
+# The main application running a simple http server with tracking capabilities.
+
+def main():
+	print("Pixel tracking image application.")
+	return 0;
+
+if (__name__ == "__main__"):
+	main()
