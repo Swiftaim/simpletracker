ac981c586126e33a0e8f6804df23d163b38ac3d1
Swiftaim <david.dalstrom@gmail.com>
2015-05-24 16:53:46 +0200

    testing within_time_interval()

--- reporting/display_report.py
+++ reporting/display_report.py
@@ -1,9 +1,11 @@
-# A simple reporting class for displaying page usage statistics.
+# A simple reporting module for displaying page usage statistics.
 
 class DisplayReport():
 	"""
+	Provides methods for producing reports on tracking data.
 	"""
-	def run_basic_report(self,from_time, to_time, data):
+
+	def generate_basic_report(self, from_time, to_time, raw_data):
 		"""
 		Runs the basic report on the supplied data.
 		
@@ -13,12 +15,55 @@ class DisplayReport():
 		data -- the data supplied to the report
 		"""
 
-		# Extract each data-entry build key statistics
-		# Key statistics are 'time', 'url', 'page views' and unique 'visitors'.
-		report = {}
-		for row in data:
-			time = row['timestamp']
-			print(time)
+		# Transform the raw data into something more reporting friendly
+		structured_data = self.build_data_structure(from_time, to_time, raw_data)
 
+		# Now build the report from the structured data.
+		report = []
+		for key, value in structured_data.items():
+			report.append([key, len(value), len(set(value))])
+
+		# Finally print the report using a pretty print method.
+		header = ['url', 'page views', 'visitors']
+		self.pretty_print_report(report, header)
+
+		# Return the actual report data for further processing.
 		return report
-			
+
+	def build_data_structure(self, from_time, to_time, raw_data):
+		"""
+		Extract all data-entries to build a more report-friendly data structure.
+		The method will also filter data based on the time interval thus exluding
+		data outside of the interval.
+		The generated data structure will be on the format {<url> : [<userid_1>...<userid_N>]}
+
+		Arguments:
+		raw_data -- the raw data provided to the report generator.
+		"""
+
+		data_structure = {}
+		for row in raw_data:
+			if self.within_time_interval(from_time, to_time, row['timestamp']):
+				if row['url'] not in data_structure:
+					data_structure[row['url']] = [row['userid']]
+				else:
+					data_structure[row['url']].append(row['userid'])
+
+		return data_structure
+
+	def within_time_interval(self, from_time, to_time, time):
+		"""Check wether a time is within a time interval or not"""
+
+		return bool((time >= from_time) and (time <= to_time))
+	
+	def pretty_print_report(self, report, header):
+		"""Prints a report in a formatted way"""
+
+		for item in header:
+			print('|{:<15}'.format(item), end='')
+		print('|')
+
+		for row in report:
+			for column in row:
+				print('|{:<15}'.format(column), end='')
+			print('|')
--- unittests/display_report_test.py
+++ unittests/display_report_test.py
@@ -17,28 +17,81 @@ import unittest
 import json
 from reporting.display_report import DisplayReport
 
-class BasicReportTest(unittest.TestCase):
+class DisplayReportTest(unittest.TestCase):
 	"""
 	Unittests for the "basic report" scenario.
 	"""
-	def setUp(self):
 
+	def setUp(self):
+		"""Setup test data"""
 		self._testdata = [
 			{ 'timestamp' : '2013-09-01 09:00:00UTC', 'url' : '/contact.html', 'userid' : '12345'},
 			{ 'timestamp' : '2013-09-01 09:00:00UTC', 'url' : '/contact.html', 'userid' : '12346'},
 			{ 'timestamp' : '2013-09-01 10:00:00UTC', 'url' : '/contact.html', 'userid' : '12345'},
-			{ 'timestamp' : '2013-09-01 11:00:00UTC', 'url' : '/contact.html', 'userid' : '12347'}
+			{ 'timestamp' : '2013-09-01 11:00:00UTC', 'url' : '/contact.html', 'userid' : '12347'},
+			{ 'timestamp' : '2013-09-01 09:00:00UTC', 'url' : '/home.html', 'userid' : '12345'},
+			{ 'timestamp' : '2013-09-01 09:00:00UTC', 'url' : '/home.html', 'userid' : '12346'},
+			{ 'timestamp' : '2013-09-01 10:00:00UTC', 'url' : '/home.html', 'userid' : '12345'},
+			{ 'timestamp' : '2013-09-01 11:00:00UTC', 'url' : '/home.html', 'userid' : '12347'}
 		]
 
 	def test_basic_report(self):
-		display_report = DisplayReport()
-		result = display_report.run_basic_report('2013-09-01 09:00:00','2013-09-01 10:59:00',self._testdata)
-		print(json.dumps(result))	
+		"""Test the basic reporting method of DisplayReport"""
 
-		assert(result['page views'] == 3)
+		# The report generating object.
+		display_report = DisplayReport()
+		# The time interval for which to generate the report
+		from_time = '2013-09-01 09:00:00UTC'
+		to_time = '2013-09-01 10:59:59UTC'
+		# Generate the report from the _testdata for the time interval [from_time -> to_time]
+		result = display_report.generate_basic_report(from_time, to_time, self._testdata)
+		# Test success criteria
+		assert(bool(result))
+		# Page views should be 3
+		assert(result[0][1] == 3)
+		# Unique visitors should be 2
+		assert(result[0][2] == 2)
+
+	def test_within_time_interval(self):
+		"""Testing the DisplayReport.within_time_interval() method"""
+
+		# The report generating object.
+		display_report = DisplayReport()
+
+		# A time interval to test with
+		from_time = '2013-09-01 09:00:00'
+		to_time = '2013-09-01 10:59:59'
+		
+		# Timestamps inside the time interval
+		time_left = '2013-09-01 10:00:00'
+		time_middle = '2013-09-01 09:37:48' 
+		time_right = '2013-09-01 10:59:59'
+
+		result = display_report.within_time_interval(from_time, to_time, time_left)
+		assert(bool(result))
+		result = display_report.within_time_interval(from_time, to_time, time_middle)
+		assert(bool(result))
+		result = display_report.within_time_interval(from_time, to_time, time_right)
+		assert(bool(result))
+
+		# Timestamps outside the time interval
+		time_before = '2013-09-01 08:59:59'
+		date_before = '2013-08-31 09:37:48' 
+		time_after = '2013-09-01 11:00:00'
+		date_after = '2014-09-01 10:00:00'
+
+		result = display_report.within_time_interval(from_time, to_time, time_before)
+		assert(not bool(result))
+		result = display_report.within_time_interval(from_time, to_time, date_before)
+		assert(not bool(result))
+		result = display_report.within_time_interval(from_time, to_time, time_after)
+		assert(not bool(result))
+		result = display_report.within_time_interval(from_time, to_time, date_after)
+		assert(not bool(result))
+
 
 	def tearDown(self):
-		print('BasicReportTest.tearDown()')
+		pass
 
 if __name__ == '__main__':
     unittest.main()
