d02f16e4248ceaf4195743ded3019056bceb254b
Swiftaim <david.dalstrom@gmail.com>
2015-05-26 18:21:25 +0200

    some minor refactoring

--- server/router.py
+++ server/router.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler
 from http import cookies
-from urllib import parse, request
 import time, uuid, os
 from reporting import display_report
 from . import logger
@@ -18,23 +17,26 @@ class Router(BaseHTTPRequestHandler):
 		delegates = {'/contact.html' : self.processContactRequest,
 						'/products.html' : self.processProductsRequest,
 						'/report.html' : self.processReportRequest}
-		#try:
-		delegates[self.path]()
-		#except:
-		#	self.page_not_found()
+		try:
+			self.render(delegates[self.path]())
+		except:
+			self.render(self.page_not_found())
 		
+
+	def render(self, view):
+		self.wfile.write(view.encode())
 
 	def processContactRequest(self):
 		"""Process the /contact request"""
 
 		# Use the simple page renderer to create the body content
-		self.wfile.write(self.render_simple_page('Contact').encode())
+		return self.render_simple_page('Contact')
 
 	def processProductsRequest(self):
 		"""Process the /products request"""
 
 		# Use the simple page renderer to create the body content
-		self.wfile.write(self.render_simple_page('Products').encode())
+		return self.render_simple_page('Products')
 
 	def processReportRequest(self):
 		# Use the simple page renderer to create the body content
@@ -45,23 +47,24 @@ class Router(BaseHTTPRequestHandler):
 		header = ['url', 'page views', 'visitors']
 		html_report = report_engine.pretty_html_report(report, header)
 		page += html_report
-		self.wfile.write(page.encode())
+		return page
 
 	def render_simple_page(self, title):
+		"""
+		Renders a simple html page with some basic css. In addition it places a
+		pixel-image in the header and puts a tracking cookie in the client 
+		browser.
+
+		Arguments:
+		title -- the page title
+		"""
 		# This is an ok route, so send 200
 		self.send_response(200)
 		self.send_header('Content-Type', 'text/html')
 
-		# Handle cookies.
-		cookie = cookies.SimpleCookie()
-		cookie_string = self.headers.get('Cookie')
-		# The first time the page is run there will be no cookies
-		if not cookie_string:
-			cookie['simpletracker-userid'] = uuid.uuid1()
-			self.send_header('Set-Cookie:', '{}'.format(cookie.output()))
-			print('place_pixel_image(), create cookie= userid:{}'.format(cookie.output().split('=',1)[1]))
-		else:
-			cookie.load(cookie_string)
+		# Get hold of the cookie, or create one if it is missing.
+		cookie = self.get_cookie()
+	
 		# All headers must be sent at this point.
 		self.end_headers()
 		
@@ -76,6 +79,20 @@ class Router(BaseHTTPRequestHandler):
 		html += "<p class=date>Server time: {} GMT</p></body></html>".format(time.asctime(time.gmtime()))
 		return html
 
+	def get_cookie(self):
+		"""Get hold of the cookie, or create one if it is missing."""
+		# Handle cookies.
+		cookie = cookies.SimpleCookie()
+		cookie_string = self.headers.get('Cookie')
+		# The first time the page is run there will be no cookies
+		if not cookie_string:
+			cookie['simpletracker-userid'] = uuid.uuid1()
+			self.send_header('Set-Cookie:', '{}'.format(cookie.output()))
+			print('place_pixel_image(), create cookie= userid:{}'.format(cookie.output().split('=',1)[1]))
+		else:
+			cookie.load(cookie_string)
+		return cookie
+
 	def read_css(self):
 		css_file = open('server/style.css')
 		style = ""
@@ -84,9 +101,15 @@ class Router(BaseHTTPRequestHandler):
 		return style
 
 	def place_pixel_image(self, id):
-		"""Injects a fake pixel image"""
-		# Log the page view
+		"""
+		Injects a fake pixel tracking image. In a real tracking application there would
+		probably be a POST to some tracking service, but since this server is tracking itself
+		why not just log the path and id.
+		"""
+
+		# Log the page view and user id for tracking purposes.
 		self.log_page_view(self.path, id)
+		# Just a fake pixel image. In a real application it would make a call to a tracking service.
 		return "<img src=data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==>"
 
 	def static_vars(**kwargs):
@@ -110,4 +133,4 @@ class Router(BaseHTTPRequestHandler):
 		self.end_headers()
 
 		html = "<html><body>Page not found!</body></html>"
-		self.wfile.write(html.encode())
+		return html
